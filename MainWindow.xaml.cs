ddf15efe2a13749291b418db4b9656fab28f9a19
Tevfik Ekici <ekicitevfik@hotmail.com>
2021-04-03 16:12:48 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DelegatesandEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.CatchEvent += W_CatchEvent;
+            w.Show();
+        }
+
+        private void W_CatchEvent(string data)
+        {
+            /* bu methodda window1 den her tetiklemede evente aktarılan veri texboxun içine yazılıyor */
+            textBox1.Text = data;
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DelegatesandEvents
+{
+    
+    public partial class Window1 : Window
+    {
+        // creating the delegate will represent an event
+        public delegate void CatchText(string data);
+
+        //  Provides delegate related event or method to be accessed by other classes by referencing its name on ram. 
+        public event CatchText CatchEvent;
+
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Every time the value of the textbox changes, the string data in it is transferred into the event named CatchEvent.
+            if (CatchEvent!=null)
+            {
+                CatchEvent(textBox1.Text);
+            }
+            
+        }
+    }
+}
